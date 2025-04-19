1488116aaf8a3baa79ff314965b0b72ea0dc5fd7
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2025-04-19 14:08:10 +0200

    create app_bar to movie_view

---
+++ lib/features/movies/presentation/views/screen/custom_sliver_to_box.dart
@@ -0,0 +1,143 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../domain/entities/movie_details.dart';
+
+class CustomSliverToBox extends StatelessWidget {
+  const CustomSliverToBox({super.key, required this.movie});
+
+  final MovieDetails movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: FadeInUp(
+        from: 20,
+        duration: const Duration(milliseconds: 500),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(movie.title,
+                  style: GoogleFonts.poppins(
+                    fontSize: 23,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 1.2,
+                  )),
+              const SizedBox(height: 8.0),
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 2.0,
+                      horizontal: 8.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[800],
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Text(
+                      movie.releaseDate.split('-')[0],
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16.0),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 20.0,
+                      ),
+                      const SizedBox(width: 4.0),
+                      Text(
+                        //يحول الرقم إلى نص فيه رقم عشري واحد فقط.
+                        (movie.voteAverage / 2).toStringAsFixed(1),
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                      const SizedBox(width: 4.0),
+                      Text(
+                        '(${movie.voteAverage})',
+                        style: const TextStyle(
+                          fontSize: 1.0,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 16.0),
+                  Text(
+                    _showDuration(movie.runTime),
+                    style: const TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Text(
+                movie.overview,
+                style: const TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 1.2,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                'Genres: ${_showGenres(movie.genres)}',
+                style: const TextStyle(
+                  color: Colors.orangeAccent,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _showGenres(List<Genre> genres) {
+    String result = '';
+    for (var genre in genres) {
+      result += '${genre.name}, ';
+    }
+
+    if (result.isEmpty) {
+      return result;
+    }
+
+    // يشيل آخر فاصلة ومسافة باستخدام substring
+    return result.substring(0, result.length - 2);
+  }
+
+  String _showDuration(int runtime) {
+
+    //hours = 130 ~/ 60 = 2
+    final int hours = runtime ~/ 60;
+    //minutes = 130 % 60 = 10
+    final int minutes = runtime % 60;
+
+    if (hours > 0) {
+      return '${hours}h ${minutes}m';
+    } else {
+      return '${minutes}m';
+    }
+  }
+}
