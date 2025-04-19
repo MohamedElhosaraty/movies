a6442680c57f1d2bc36c1906d1d265856010eb17
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2025-04-19 14:08:59 +0200

    create top_rate to movie_view

---
+++ lib/features/movies/presentation/views/screen/custom_popular_listview_image.dart
@@ -0,0 +1,89 @@
+import 'dart:developer';
+
+import 'package:animate_do/animate_do.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies/core/utils/end_point.dart';
+import 'package:movies/features/movies/presentation/controller/movies_bloc.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../../../../../core/utils/enum.dart';
+import '../../controller/movies_state.dart';
+import '../movie_detail_screen.dart';
+
+class CustomPopularListviewImage extends StatelessWidget {
+  const CustomPopularListviewImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MoviesBloc, MoviesState>(
+      buildWhen: (previous, current) =>
+          previous.popularState != current.popularState,
+      builder: (context, state) {
+        switch (state.popularState) {
+          case RequestState.loading:
+            return const SizedBox(
+              height: 170,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          case RequestState.loaded:
+            return FadeIn(
+              duration: const Duration(milliseconds: 500),
+              child: SizedBox(
+                height: 170.0,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  itemCount: state.popularMovies.length,
+                  itemBuilder: (context, index) {
+                    final movie = state.popularMovies[index];
+                    return Container(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: InkWell(
+                        onTap: () {
+                          /// TODO : NAVIGATE TO  MOVIE DETAILS
+                          log ("movie.id : ${movie.id}");
+                           Navigator.pushNamed(context, MovieDetailScreen.routeName,arguments: movie.id);
+                        },
+                        child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8.0)),
+                          child: CachedNetworkImage(
+                            width: 120.0,
+                            fit: BoxFit.cover,
+                            imageUrl: EndPoint.imageUrl(movie.backdropPath),
+                            placeholder: (context, url) => Shimmer.fromColors(
+                              baseColor: Colors.grey[850]!,
+                              highlightColor: Colors.grey[800]!,
+                              child: Container(
+                                height: 170.0,
+                                width: 120.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          case RequestState.error:
+            return Center(child: Text(state.popularMessage));
+        }
+      },
+    );
+  }
+}
---
+++ lib/features/movies/presentation/views/screen/custom_sliver_to_recommendation.dart
@@ -0,0 +1,81 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies/features/movies/domain/entities/movie_recommendation.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../../../../../core/utils/dummy_details.dart';
+import '../../../../../core/utils/end_point.dart';
+import '../../../../../core/utils/enum.dart';
+import '../../controller/movie_details_bloc.dart';
+
+class CustomSliverToRecommendation extends StatelessWidget {
+  const CustomSliverToRecommendation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MovieDetailsBloc, MovieDetailsState>(
+      builder: (context, state) {
+
+        switch (state.movieRecommendationState) {
+          case RequestState.loading:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          case RequestState.loaded:
+            return SliverPadding(
+              padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 24.0),
+              sliver: _showRecommendations(state.movieRecommendation ?? recommendationDummy),
+            );
+          case RequestState.error:
+            return Text(state.movieDetailsMessage);
+        }
+      },
+    );
+  }
+
+  Widget _showRecommendations( List<MovieRecommendation> recommendations) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+            (context, index) {
+          final recommendation = recommendations[index];
+          return FadeInUp(
+            from: 20,
+            duration: const Duration(milliseconds: 500),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+              child: CachedNetworkImage(
+                imageUrl: EndPoint.imageUrl(recommendation.backdropPath!),
+                placeholder: (context, url) =>
+                    Shimmer.fromColors(
+                      baseColor: Colors.grey[850]!,
+                      highlightColor: Colors.grey[800]!,
+                      child: Container(
+                        height: 170.0,
+                        width: 120.0,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                height: 180.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+        childCount: recommendations.length,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        mainAxisSpacing: 8.0,
+        crossAxisSpacing: 8.0,
+        childAspectRatio: 0.7,
+        crossAxisCount: 3,
+      ),
+    );
+  }
+
+}
